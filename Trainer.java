ce2fc20adc5d3d6540f56cb54a19b509b47782a4
biruktawitendale <biruktawitendale316@gmail.com>
2026-06-22 19:21:14 +0300

    Add constructor and methods to Trainer class

--- Trainer.java
+++ Trainer.java
@@ -10,3 +10,25 @@ public class Trainer extends Person {
         super(generateTrainerId(), name, age, phone, ROLE);
         this.specialty = specialty;
     }
+// Loading from database — ID is provided
+    public Trainer(String id, String name, int age, String phone, String specialty) throws GymException {
+        super(id, name, age, phone, ROLE);
+        this.specialty = specialty;
+    }
+
+    public String getSpecialty() { return specialty; }
+
+    @Override
+    public String getRole() { return ROLE; }
+
+    @Override
+    public String getDetails() {
+        return toString();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()        + "\n" +
+               "Specialty: " + specialty;
+    }
+}
